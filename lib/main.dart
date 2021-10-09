ccbe3eb171d59f41ac26e6316e28b2f2283e7cbd
Ehsan Ullah <ehsanullah184wb@gmail.com>
2021-10-09 07:45:25 +0500

    flutter_practice
    just doing practice in flutter app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Sample",
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("My APP"),
+            ),
+            body: Center(
+              child: Text("Ehsan Ullah",
+                style: TextStyle(
+                  color: Colors.tealAccent,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 40.0,
+                  background: Paint()..color=Colors.black..style=PaintingStyle.stroke,
+                ),
+
+              ),
+            )
+        )
+    );
+  }
+}
