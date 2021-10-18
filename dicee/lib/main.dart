a2810c99ed5ce71556c8c9db98a926b532d6858d
Ehsan Ullah <ehsanullah184wb@gmail.com>
2021-10-18 13:59:55 +0500

    dice code

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,113 +1,155 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MaterialApp(
+    title: "Dice App",
+    color: Colors.red,
+    home: homePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  int num1 = 1;
+  int num2 = 1;
+  int num3 = 1;
+  int num4 = 1;
+  int pls1 = 0;
+  int pls2 = 0;
+  int pls3 = 0;
+  int pls4 = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.red,
+      body: SafeArea(
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "Player 1",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 160),
+                  child: Text(
+                    "Player 2",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      num1 = Random().nextInt(6) + 1;
+                      pls1 += num1;
+                    });
+                  },
+                  child: Image.asset(
+                    "images/dice$num1.png",
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+                SizedBox(
+                  width: 100,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      num2 = Random().nextInt(6) + 1;
+                      pls2 += num2;
+                    });
+                  },
+                  child: Image.asset(
+                    "images/dice$num2.png",
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+              ],
             ),
+            SizedBox(
+              height: 100,
+            ),
+
+            Text("Player 1 score: $pls1"),
+            Text("Player 2 score: $pls2"),
+            Text("Player 3 score: $pls3"),
+            Text("Player 4 score: $pls4"),
+
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      num3 = Random().nextInt(6) + 1;
+                      pls3 += num3;
+                    });
+                  },
+                  child: Image.asset(
+                    "images/dice$num3.png",
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+                SizedBox(
+                  width: 100,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      num4 = Random().nextInt(6) + 1;
+                      pls4 += num4;
+                    });
+                  },
+                  child: Image.asset(
+                    "images/dice$num4.png",
+                    height: 150,
+                    width: 150,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "Player 3",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 160),
+                  child: Text(
+                    "Player 4",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                )
+              ],
+            ),
+
+
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
