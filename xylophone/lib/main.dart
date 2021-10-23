a4e1f927ac288846b26ba509a8dd685fac11d93c
Ehsan Ullah <ehsanullah184wb@gmail.com>
2021-10-23 15:40:21 +0500

    Update main.dart

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,146 +1,53 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main()=> runApp(MaterialApp(
-  theme: ThemeData(primaryColor: Colors.red,accentColor: Colors.yellowAccent),
-  debugShowCheckedModeBanner: false,
-  home: xylo(),
-)
-);
-
-class xylo extends StatefulWidget{
-  @override
-  SplashScreenState createState()=> SplashScreenState();
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(child: MyApp()),
+    ),
+  ));
 }
 
-class SplashScreenState extends State<xylo>{
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Timer(Duration(seconds: 5),()=>Navigator.pushReplacement(context,
-        MaterialPageRoute(builder:
-        (context) => xylophone()
-    )
-    )
-    );
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  void playAudio(String fileName) {
+    final players = AudioCache();
+    players.play('assets_note$fileName.wav');
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.orangeAccent),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 100.0,
-                        child: Icon(
-                          Icons.android,
-                          color: Colors.orangeAccent,
-                          size: 100.0,
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(top: 40.0),
-                      ),
-                      Text(
-                        "Flutter",
-                        style: TextStyle(color: Colors.white,
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text("Splash Screen",
-                        style: TextStyle(color: Colors.white,
-                          fontSize: 30.0,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation(Colors.tealAccent),
-                        strokeWidth: 5.0,
-                        //backgroundColor: Colors.redAccent,
-                      ),
-                      height: 70.0,
-                      width: 70.0,
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 40.0),
-
-                    ),
-                    Text("Welcome\nEveryone",
-                      style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold),
-
-                    )
-                  ],
-                ),)
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-}
-class xylophone extends StatelessWidget {
-  void playSound(int soundNumber) {
-    final player = AudioCache();
-    player.play('note$soundNumber.wav');
-  }
-
-  Expanded buildKey({Color color, int soundNumber}) {
+  Expanded customWidget(Color color, String fileName) {
     return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () {
-          playSound(soundNumber);
-        },
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.purple, soundNumber: 7),
-            ],
+      child: Container(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: color,
           ),
+          onPressed: () {
+            playAudio(fileName);
+          },
+          child: Text(''),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        customWidget(Colors.red, "1"),
+        customWidget(Colors.grey, "2"),
+        customWidget(Colors.green, "3"),
+        customWidget(Colors.blue, "4"),
+        customWidget(Colors.cyan, "5"),
+        customWidget(Colors.yellow, "6"),
+        customWidget(Colors.pink, "7"),
+      ],
     );
   }
 }
