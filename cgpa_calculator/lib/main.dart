acf5644b7d9d1864bb514b47ebaae84703a0bf9d
Ehsan Ullah <ehsanullah184wb@gmail.com>
2021-11-10 09:17:35 +0500

    Update main.dart

--- cgpa_calculator/lib/main.dart
+++ cgpa_calculator/lib/main.dart
@@ -12,7 +12,33 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
-      home: InputPage(),
+      home: splash_screen(),
+    );
+  }
+}
+class splash_screen extends StatelessWidget {
+  const splash_screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Ehsan",
+              style: TextStyle(fontWeight: FontWeight.bold,
+              color: Colors.amber,
+                fontSize: 50.0,
+
+              ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
