19252386d46ec99ca21902bf57475e88a96cd4b4
Ehsan Ullah <ehsanullah184wb@gmail.com>
2021-11-25 21:20:24 +0500

    Update next.dart

--- gp_fund_cal/lib/next.dart
+++ gp_fund_cal/lib/next.dart
@@ -75,28 +75,81 @@ class MyCustomForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter your slaray',
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your slaray',
+                ),
+              ),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your username',
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: MaterialButton(onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => abc()),
+                );
+              },
+                splashColor: Colors.red,
+                child: Text("Done",
+                  style: TextStyle(
+                    fontSize: 20.0,
+
+
+
+                  ),
+                ),
+              )
             ),
+          ],
+        )
+      ),
+    );
+  }
+}
+class abc extends StatefulWidget {
+  const abc({Key? key}) : super(key: key);
+
+  @override
+  _abcState createState() => _abcState();
+}
+
+class _abcState extends State<abc> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Actual Value"),
           ),
-        ),
-      ],
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Row(
+                      children: [
+                        Text("Your interseted salary is 33300 for year one for year 2 select og-2",
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            color: Colors.cyan,
+                          ),
+                        ),
+                      ]
+                  ),
+                                 ],
+              ),
+            ],
+          )
+      ),
     );
   }
 }
