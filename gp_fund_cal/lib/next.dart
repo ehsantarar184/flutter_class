e485a1cdbe1d1521004745c2c0800d719a9ef922
Ehsan Ullah <ehsanullah184wb@gmail.com>
2021-11-25 21:11:28 +0500

    Update next.dart

--- gp_fund_cal/lib/next.dart
+++ gp_fund_cal/lib/next.dart
@@ -65,6 +65,38 @@ class interset extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MyCustomForm(),
+    );
+  }
+}
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your slaray',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your username',
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
